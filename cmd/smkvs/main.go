f047d1ffdf1934398a067bc684a81fd2a2f95c16
LaCodon <mf-mm@t-online.de>
2020-07-27 11:50:08 +0200

    Fix merge stupidities

--- cmd/smkvs/main.go
+++ cmd/smkvs/main.go
@@ -19,20 +19,11 @@ func main() {
 		Usage:       "Schmidt-Maier-Key-Value-Store",
 		Version:     Version,
 		Description: "Distributed Key Value Store based on the raft algorithm",
-		Action:      run,
-		Flags: []cli.Flag{
-			&cli.StringSliceFlag{
-				Name:        "peer-ip",
-				Aliases:     []string{"i"},
-				Usage:       "Peer IPs and Ports in",
-				EnvVars:     []string{"SMKVS_PEER_NODES"},
-				Required:    true,
-				Destination: config.Default.AllNodes,
 		Commands: []*cli.Command{
 			{
-				Name: "run",
-				Usage: "Run a smkvs node",
-				Action:      run,
+				Name:   "run",
+				Usage:  "Run a smkvs node",
+				Action: run,
 				Flags: []cli.Flag{
 					&cli.StringSliceFlag{
 						Name:        "peer-ip",
@@ -61,17 +52,9 @@ func main() {
 					},
 				},
 			},
-			&cli.IntFlag{
-				Name:        "local-port",
-				Aliases:     []string{"p"},
-				Usage:       "RPC server listen port",
-				EnvVars:     []string{"SMKVS_PORT"},
-				Value:       36000,
-				Destination: &config.Default.LocalPort,
-
 			{
-				Name: "set",
-				Usage: "Store a new key value pair",
+				Name:   "set",
+				Usage:  "Store a new key value pair",
 				Action: setKVPair,
 				Flags: []cli.Flag{
 					&cli.StringFlag{
@@ -85,29 +68,21 @@ func main() {
 						Name:        "key",
 						Aliases:     []string{"k"},
 						Usage:       "Value is stored to this key",
-						Required:	 true,
+						Required:    true,
 						Destination: &userInput.Key,
 					},
 					&cli.StringFlag{
 						Name:        "value",
 						Aliases:     []string{"v"},
 						Usage:       "Value to be stored",
-						Required:	 true,
+						Required:    true,
 						Destination: &userInput.Var,
 					},
 				},
 			},
-			&cli.StringFlag{
-				Name:        "listen",
-				Aliases:     []string{"l"},
-				Usage:       "External listener IP and Port for current node",
-				EnvVars:     []string{"SMKVS_NODE_LISTEN"},
-				Required:    true,
-				Value:       "127.0.0.1:36000",
-				Destination: &config.Default.MyNode,
 			{
-				Name: "delete",
-				Usage: "Delete a stored key value pair",
+				Name:   "delete",
+				Usage:  "Delete a stored key value pair",
 				Action: deleteKVPair,
 				Flags: []cli.Flag{
 					&cli.StringFlag{
@@ -121,14 +96,14 @@ func main() {
 						Name:        "key",
 						Aliases:     []string{"k"},
 						Usage:       "Value is stored to this key",
-						Required:	 true,
+						Required:    true,
 						Destination: &userInput.Key,
 					},
 				},
 			},
 			{
-				Name: "get",
-				Usage: "Get all current key value pairs stored in the system.",
+				Name:   "get",
+				Usage:  "Get all current key value pairs stored in the system.",
 				Action: getStorage,
 				Flags: []cli.Flag{
 					&cli.StringFlag{
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-PEERS=("10.0.0.11:36000" "10.0.0.12:36000" "10.0.0.13:36000" "10.0.0.14:36000" "10.0.0.15:36000")
+PEERS=("10.0.0.11:36000" "10.0.0.12:36001" "10.0.0.13:36002" "10.0.0.14:36003" "10.0.0.15:36004")
 num=$1
-
 
 if [[ "$num" == "" ]]; then
   if [[ "$NODE_ID" != "" ]]; then
@@ -10,24 +9,29 @@ if [[ "$num" == "" ]]; then
   else
     num=1
   fi
+else
+  PEERS=("127.0.0.1:36000" "127.0.0.1:36001" "127.0.0.1:36002" "127.0.0.1:36003" "127.0.0.1:36004")
 fi
-
-
-PORT=36000
 
 case "$num" in
 1)
-  ./bin/smkvs run -p $PORT -l "${PEERS[0]}" -i "${PEERS[1]}"  ;;
+  PORT=36000
+  ./bin/smkvs run -p $PORT -l "${PEERS[0]}" -i "${PEERS[1]}"
+  ;;
 2)
+  PORT=36001
   ./bin/smkvs run -p $PORT -l "${PEERS[1]}" -i "${PEERS[0]}"
   ;;
 3)
+  PORT=36002
   ./bin/smkvs run -p $PORT -l "${PEERS[2]}" -i "${PEERS[0]}"
   ;;
 4)
+  PORT=36003
   ./bin/smkvs run -p $PORT -l "${PEERS[3]}" -i "${PEERS[0]}"
   ;;
 5)
+  PORT=36004
   ./bin/smkvs run -p $PORT -l "${PEERS[4]}" -i "${PEERS[0]}"
   ;;
 esac
